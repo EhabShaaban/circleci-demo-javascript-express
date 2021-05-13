b849c17191616f52a5b13e14af002e8aa828bbf8
Ehab Shaaban <ehab.shaaban@kites-software.com>
2021-05-13 21:34:23 +0200

    /

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,6 @@
 var http = require("http");
 var server = http.createServer(function (req, res) {
 res.writeHead(200);
-res.end("Hello world! from ec2");
+res.end("Hello world! spitted out of aws ec2");
 });
 server.listen(3000);
